94d6c6a422174725953a1de1f8341174ed7bc12d
Rok Kralj <info@rok-kralj.net>
2015-08-11 19:05:50 +0200

    Completed native naive KNN classifier.

--- src/main/scala/qlearn/ml/classify/FittedKNN.scala
+++ src/main/scala/qlearn/ml/classify/FittedKNN.scala
@@ -1,6 +1,7 @@
 package qlearn.ml.classify
 
-import qlearn.Types.Mat
+import breeze.linalg.{normalize, sum}
+import qlearn.Types._
 import qlearn.dataset.{Nominal, Unlabeled}
 import qlearn.loss.numerical.distance.{EuclideanDistance, Distance}
 import qlearn.ml.FittedModel
@@ -9,10 +10,49 @@ case class FittedKNN(
 	schema: Nominal,
 	k: Int,
 	distance: Distance = EuclideanDistance,
-	weighting: Double => Double = identity
+	weighting: Double => Double = {_ => 1.0}
 ) extends FittedModel[Nominal] {
 
-	require(k <= schema.recordCount, "Cannot run kNN classifier on a dataset with less than k records.")
+	/*
+		This classifier predicts the class distribution probabilities according
+		to the k nearest records and their actual distance.
 
-	def predict(data: Unlabeled) = ???
+		With the weighting function you can denote how you want to ponderize
+		the records according to their distance. Some examples:
+
+		{_ => 1.0}      : ponderize the points completely equally
+		math.pow(_, -p) : exponential dampering for p > 0
+		...
+	 */
+
+	require(k <= schema.recordCount, s"Cannot run $k-NN classifier on a dataset with just ${schema.recordCount} records.")
+
+	/*
+		This function returns the indices of k smallest
+		elements of a vector.
+
+		TODO: Sorting is slow, rewrite to MoM algorithm.
+	 */
+
+	private def kSmallest(distances: Vec, k: Int) = {
+		val vec = distances.toScalaVector
+		vec.indices.sortBy(vec).take(k)
+	}
+
+	def predict(data: Unlabeled) = {
+		val newy = data.xmat.r.map { record =>
+			val distances = distance(schema.xmat, record)
+			val smallest = kSmallest(distances, k)
+
+			// select k smallest distances and weight them
+			val weighted = distances(smallest).map(weighting)
+			// select k closest corresponding output rows
+			val chosen = schema.ymat(smallest, ::).toDenseMatrix
+			// weight those rows with "weighted" vector
+			val result = sum((chosen.c :* weighted).c).toDenseVector
+			// finally, normalize
+			result / sum(result)
+		}
+		schema.updated(data, newy)
+	}
 }
