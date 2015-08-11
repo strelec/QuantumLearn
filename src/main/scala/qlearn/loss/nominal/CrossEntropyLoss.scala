03790dd155224e0ecef481da4494c075ff5e5662
Rok Kralj <info@rok-kralj.net>
2015-08-11 20:10:47 +0200

    Added the infinity margin for CrossEntropyLoss.

--- src/main/scala/qlearn/loss/nominal/CrossEntropyLoss.scala
+++ src/main/scala/qlearn/loss/nominal/CrossEntropyLoss.scala
@@ -1,12 +1,14 @@
 package qlearn.loss.nominal
 
-import breeze.linalg.sum
+import breeze.linalg.{min, max, sum}
 import breeze.numerics.log
 import qlearn.dataset.Nominal
 import qlearn.loss.Loss
 
-case class CrossEntropyLoss(margin: Double = 1e-10) extends Loss[Nominal] {
-	// TODO: implement margin
-	def apply(actual: Nominal, predicted: Nominal) =
-		sum(log(predicted.ymat) :* actual.ymat) / -actual.recordCount
+case class CrossEntropyLoss(margin: Double = 1e-15) extends Loss[Nominal] {
+
+	def apply(actual: Nominal, predicted: Nominal) = {
+		val corrected = min(max(predicted.ymat, margin), 1 - margin)
+		sum(log(corrected) :* actual.ymat) / -actual.recordCount
+	}
 }
